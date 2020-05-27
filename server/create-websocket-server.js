05d45ca3a8f75ef82dc2c4ac832090675e716c5f
Devyaterikov Petr <p.devyaterkov@ifrbanking.ru>
2020-05-27 21:18:06 +0300

    Adds users list messages support on server

--- server/create-websocket-server.js
+++ server/create-websocket-server.js
@@ -1,58 +1,90 @@
 import WebSocket from 'ws';
 
-const createWebsocketServer = (clients) => {
-  const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+const KEEP_ALIVE_INTERVAL = 1000 * 30; // 30 sec
+const MAX_MESSAGE_LENGTH = 2000;
+const MessageType = {
+  USER_MESSAGE: 'USER_MESSAGE',
+  BROADCAST_MESSAGE: 'BROADCAST_MESSAGE',
+  USERS_LIST: 'USERS_LIST',
+};
 
-  function heartbeat() {
-    this.isAlive = true;
-  }
+function heartbeat() {
+  this.isAlive = true;
+}
 
-  const broadcast = (data) => {
-    const json = JSON.stringify(data);
-
-    clients.forEach((ws) => {
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.send(json);
-      }
-    });
-  };
-
-  const messageHandler = (req) => (json) => {
-    req.session.touch();
-    req.session.save();
-
-    const data = JSON.parse(json);
-
-    switch (data.type) { // eslint-disable-line default-case
-      case 'SEND_MESSAGE': {
-        const message = { timestamp: Date.now(), message: data.payload };
-        broadcast(message);
-      }
-    }
-  };
-
-  wss.on('connection', (ws, req) => {
-    const { id } = req.session;
-    clients.set(id, ws);
-
-    ws.isAlive = true; // eslint-disable-line no-param-reassign
-    ws.on('pong', heartbeat);
-    ws.on('message', messageHandler(req));
-    ws.on('close', () => { clients.delete(id); });
-  });
-
+const enableKeepAliveChecking = (clients) => {
   const interval = setInterval(() => {
-    clients.forEach((ws) => { // eslint-disable-line consistent-return
+    clients.forEach(({ ws }) => { // eslint-disable-line consistent-return
       if (ws.isAlive === false) {
-        return ws.terminate();
+        return ws.close();
       }
       ws.isAlive = false; // eslint-disable-line no-param-reassign
       ws.ping();
     });
-  }, 30000);
+  }, KEEP_ALIVE_INTERVAL);
+  return () => clearInterval(interval);
+};
 
+const broadcast = (data, clients) => {
+  const json = JSON.stringify(data);
+
+  clients.forEach(({ ws }) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(json);
+    }
+  });
+};
+
+const sendActualUsersList = (clients) => {
+  const usersList = [...clients].map(([id, { name }]) => ({ id, name }));
+  const message = {
+    type: MessageType.USERS_LIST,
+    payload: usersList,
+  };
+  broadcast(message, clients);
+};
+
+const messageHandler = (json, clients) => {
+  const data = JSON.parse(json);
+
+  switch (data.type) { // eslint-disable-line default-case
+    case MessageType.USER_MESSAGE: {
+      const userMessage = data.payload.length <= MAX_MESSAGE_LENGTH
+        ? data.payload
+        : data.payload.slice(0, MAX_MESSAGE_LENGTH);
+      const message = {
+        type: MessageType.BROADCAST_MESSAGE,
+        payload: {
+          timestamp: Date.now(),
+          message: userMessage,
+        },
+      };
+      broadcast(message, clients);
+    }
+  }
+};
+
+const createWebsocketServer = (clients) => {
+  const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+  const disableKeepAliveChecking = enableKeepAliveChecking(clients);
+
+  wss.on('connection', (ws, req) => {
+    const { id, name } = req.session;
+    clients.set(id, { ws, name });
+    ws.isAlive = true; // eslint-disable-line no-param-reassign
+    sendActualUsersList(clients);
+
+    ws.on('pong', heartbeat);
+    ws.on('message', (evt) => {
+      messageHandler(evt, clients);
+    });
+    ws.on('close', () => {
+      clients.delete(id);
+      sendActualUsersList(clients);
+    });
+  });
   wss.on('close', () => {
-    clearInterval(interval);
+    disableKeepAliveChecking();
   });
 
   return wss;
